1615f6aa90af261b7c85dd6958d9160d94022a92
kkook1234 <57415a@naver.com>
2023-10-11 21:32:23 +0900

    docs: Ratingsystem.css 수정

--- frontend/src/ratingsystem/pages/Ratingsystem.js
+++ frontend/src/ratingsystem/pages/Ratingsystem.js
@@ -9,42 +9,44 @@ const Ratingsystem = () => {
       <Pagetitle title="등급제">
         파손 부위 별로 등급을 판단할 수 있어요
       </Pagetitle>
-      <div className="ratingsystem_ratingcontain ">
-        <Ratinginfo
-          color="#759CFF"
-          backgroundColor="#F0F4FF"
-          percent="5"
-          fontcolor="#759CFF"
-          rating="SS"
-        />
-        <Ratinginfo
-          color="#759CFF"
-          backgroundColor="#759CFF"
-          percent="10"
-          fontcolor="#ffffff"
-          rating="S"
-        />
-        <Ratinginfo
-          color="#FFC806"
-          backgroundColor="#FFC806"
-          percent="30"
-          fontcolor="#ffffff"
-          rating="A"
-        />
-        <Ratinginfo
-          color="#B3B3B3"
-          backgroundColor="#B3B3B3"
-          percent="50"
-          fontcolor="#ffffff"
-          rating="B"
-        />
-        <Ratinginfo
-          color="#DDCC93"
-          backgroundColor="#DDCC93"
-          percent="70"
-          fontcolor="#ffffff"
-          rating="C"
-        />
+      <div className="mobile-contain">
+        <div className="ratingsystem_ratingcontain ">
+          <Ratinginfo
+            color="#759CFF"
+            backgroundColor="#F0F4FF"
+            percent="5"
+            fontcolor="#759CFF"
+            rating="SS"
+          />
+          <Ratinginfo
+            color="#759CFF"
+            backgroundColor="#759CFF"
+            percent="10"
+            fontcolor="#ffffff"
+            rating="S"
+          />
+          <Ratinginfo
+            color="#FFC806"
+            backgroundColor="#FFC806"
+            percent="30"
+            fontcolor="#ffffff"
+            rating="A"
+          />
+          <Ratinginfo
+            color="#B3B3B3"
+            backgroundColor="#B3B3B3"
+            percent="50"
+            fontcolor="#ffffff"
+            rating="B"
+          />
+          <Ratinginfo
+            color="#DDCC93"
+            backgroundColor="#DDCC93"
+            percent="70"
+            fontcolor="#ffffff"
+            rating="C"
+          />
+        </div>
       </div>
       <img
         className="ratingsystem_image"
