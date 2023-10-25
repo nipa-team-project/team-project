2931cbd85c4eedd1ce67aa20abd0992d8d1cc06f
leesuuuuumm <tnalsvjscl@naver.com>
2023-10-25 23:53:13 +0900

    chore: db config file 추가

--- backend/app/db/database.py
+++ backend/app/db/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
-DB_URL = "mysql+pymysql://root:password@127.0.0.1:3306/refurlab?charset=utf8mb4"
+DB_URL = ""
 engine = create_engine(DB_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
@@ -13,4 +13,3 @@ def get_db():
         yield db
     finally:
         db.close()
-
