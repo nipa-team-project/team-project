9ef84ec7020f7a222b061128276a376bcbb073d8
kkook1234 <57415a@naver.com>
2023-10-25 23:04:19 +0900

    first commit

---
+++ frontend/src/main/pages/Main.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import "./Main.css";
+
+const Main = () => {};
+
+export default Main;
---
+++ frontend/src/shared/Navigation/AuthLinks.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+import "./AuthLinks.css";
+
+const AuthLinks = (props) => {
+  const login = props.login;
+  return (
+    <React.Fragment>
+      {!login ? (
+        <>
+          <li className="header_links_auth_list">
+            <NavLink to="/">회원가입</NavLink>
+          </li>
+          <span className="auth_vector"></span>
+          <li className="header_links_auth_list">
+            <NavLink to="/main" onClick={props.logintrue}>
+              로그인
+            </NavLink>
+          </li>
+        </>
+      ) : (
+        <>
+          <li className="header_links_auth_list">
+            <NavLink to="/">내 정보 수정</NavLink>
+          </li>
+          <span className="auth_vector"></span>
+          <li className="header_links_auth_list">
+            <NavLink to="/main" onClick={props.loginfalse}>
+              로그아웃
+            </NavLink>
+          </li>
+        </>
+      )}
+    </React.Fragment>
+  );
+};
+
+export default AuthLinks;
