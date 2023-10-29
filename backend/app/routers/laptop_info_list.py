e1fac984e19b57d4e3b5b84186ca2e2a6ae587bb
찬익 박 <piaocanyi1997@gmail.com>
2023-10-29 22:25:47 +0900

    feat: routers/laptop_info_list.py 수정

--- backend/app/routers/laptop_info_list.py
+++ backend/app/routers/laptop_info_list.py
@@ -9,6 +9,7 @@ import logging
 router = APIRouter()
 logger = logging.getLogger(__name__)
 
+
 # @router.post("/laptop_info_list/", response_model=schemas.Laptop)
 # def create_laptop(laptop: schemas.LaptopCreate, db: Session = Depends(db.session)):
 #     return crud.create_laptop(db=db, laptop=laptop)
@@ -17,12 +18,11 @@ logger = logging.getLogger(__name__)
 @router.get("/purchase/goods/", response_model=List[schemas.Laptop])
 def read_laptops(skip: int = 0, limit: int = 100, db: Session = Depends(db.session)):
     try:
-        laptops = crud.get_laptops(db,skip=skip, limit=limit)
+        laptops = crud.get_laptops(db, skip=skip, limit=limit)
         return laptops
     except Exception as e:
         print(logger.error(e))
         raise HTTPException(status_code=500, detail="An error occurred.")
-
 
 
 # @router.get("/purchase/goods/details/{laptop_id}", response_model=schemas.Laptop)
@@ -32,7 +32,7 @@ def read_laptops(skip: int = 0, limit: int = 100, db: Session = Depends(db.sessi
 #         raise HTTPException(status_code=404, detail="Laptop not found")
 #     return db_laptop
 
-@router.get("/purchase/goods/details/{laptop_id}/", response_model=List[schemas.Image])
+@router.post("/purchase/goods/details/{laptop_id}/", response_model=List[schemas.Image])
 def read_images(laptop_id: int, db: Session = Depends(db.session)):
     # 데이터베이스에서 laptop_id에 해당하는 Laptop을 조회합니다.
     db_laptop = crud.get_laptop(db, laptop_id=laptop_id)
--- frontend/src/main/pages/Main.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Link } from "react-router-dom";
-
-import "./Main.css";
-
-const Main = () => {};
-
-export default Main;
