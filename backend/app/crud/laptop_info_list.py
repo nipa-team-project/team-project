ce1616b009d36ac8e305b09d3e83f28a7a449895
찬익 박 <piaocanyi1997@gmail.com>
2023-11-04 00:10:09 +0900

    feat: paging parameter 변경, desc&asc 수정

--- backend/app/crud/laptop_info_list.py
+++ backend/app/crud/laptop_info_list.py
@@ -1,39 +1,32 @@
+from fastapi import HTTPException
 from sqlalchemy.orm import Session, joinedload
 from models import laptop_info_list as models
 from schemas import laptop_info_list as schemas
 
 
-# def get_laptop(db: Session, laptop_id: int):
-#     return db.query(models.Laptop).filter(models.Laptop.laptop_info_list_id == laptop_id).first()
-
-def get_laptop(db: Session, laptop_id: int):
-    return db.query(models.Image).filter(models.Image.laptop_info_list_id == laptop_id).all()
-
-
-def get_laptops(db: Session, skip: int = 0, limit: int = 6):
-    return db.query(models.Laptop).options(joinedload(models.Laptop.laptop_info_list_image)).offset(skip).limit(
-        limit).all()
-
-
-def get_laptops_asc(db: Session, skip: int = 0, limit: int = 6, rating=str):
+def get_laptops_desc(db: Session, page: int = 1, rating=str):
+    page_size = 6
+    skip = (page - 1) * page_size
     return (
         db.query(models.Laptop)
         .options(joinedload(models.Laptop.laptop_info_list_image))
-        .filter(models.Laptop.rank == rating)  # 'rating' 조건 추가
-        .order_by(models.Laptop.create_date.asc())  # 오름차순 추가
+        .filter(models.Laptop.rank == rating)
+        .order_by((models.Laptop.price.desc()))
         .offset(skip)
-        .limit(limit)
+        .limit(page_size)
         .all()
     )
 
 
-def get_laptops_desc(db: Session, skip: int = 0, limit: int = 6, rating=str):
+def get_laptops_asc(db: Session, page: int = 1, rating=str):
+    page_size = 6
+    skip = (page - 1) * page_size
     return (
         db.query(models.Laptop)
         .options(joinedload(models.Laptop.laptop_info_list_image))
-        .filter(models.Laptop.rank == rating)  # 'rating' 조건 추가
-        .order_by(models.Laptop.create_date.desc())  # 오름차순 추가
+        .filter(models.Laptop.rank == rating)
+        .order_by((models.Laptop.price.asc()))
         .offset(skip)
-        .limit(limit)
+        .limit(page_size)
         .all()
     )
--- backend/app/routers/laptop_info_list.py
+++ backend/app/routers/laptop_info_list.py
@@ -1,5 +1,5 @@
 from typing import List
-from fastapi import APIRouter, Depends, HTTPException,Response
+from fastapi import APIRouter, Depends, HTTPException, Response
 from sqlalchemy.orm import Session
 
 from crud import laptop_info_list as crud
@@ -11,27 +11,11 @@ router = APIRouter()
 logger = logging.getLogger(__name__)
 
 
-# @router.get("/purchase/goods/", response_model=List[schemas.Laptop])
-# def read_laptops(skip: int = 0, limit: int = 10, db: Session = Depends(db.session)):
-#     laptops = crud.get_laptops(db, skip=skip, limit=limit)
-#     return laptops
-
-# @router.get("/purchase/goods/", response_model=List[schemas.Laptop])
-# def read_laptops(skip: int = 0, limit: int = 10, db: Session = Depends(db.session)):
-#     try:
-#         laptops = crud.get_laptops(db, skip=skip, limit=limit)
-#         if laptops is None:
-#             raise ValueError
-#         return Response(content=laptops, status_code=200)
-#     except ValueError:
-#         raise HTTPException(status_code=404, detail="Laptops not found")
-#     except Exception:
-#         raise HTTPException(status_code=500, detail="Internal Server Error")
-
-@router.get("/purchase/goods/", response_model=List[schemas.Laptop])
-def read_laptops(skip: int = 0, limit: int = 6, db: Session = Depends(db.session)):
+# ex) http://localhost:8000/purchase/goods/desc?page=1&rating=s
+@router.get("/purchase/goods/desc", response_model=List[schemas.Laptop])
+def read_laptops(page: int = 1, rating: str = None, db: Session = Depends(db.session)):
     try:
-        laptops = crud.get_laptops(db, skip=skip, limit=limit)
+        laptops = crud.get_laptops_desc(db, page=page, rating=rating)
         if laptops is None or len(laptops) == 0:
             raise ValueError
         return laptops
@@ -41,29 +25,11 @@ def read_laptops(skip: int = 0, limit: int = 6, db: Session = Depends(db.session
         raise HTTPException(status_code=500, detail="Internal Server Error")
 
 
-# @router.post("/purchase/{laptop_id}/goods/details/", response_model=List[schemas.Image])
-# def get_laptop_images(laptop_id: int, db: Session = Depends(db.session)):
-#     images = crud.get_laptop(db, laptop_id=laptop_id)
-#     return images
-
-
-@router.post("/purchase/{laptop_id}/goods/details/", response_model=List[schemas.Image])
-def get_laptop_images(laptop_id: int, db: Session = Depends(db.session)):
+# ex) http://localhost:8000/purchase/goods/asc?page=1&rating=s
+@router.get("/purchase/goods/asc", response_model=List[schemas.Laptop])
+def read_laptops(page: int = 1, rating: str = None, db: Session = Depends(db.session)):
     try:
-        images = crud.get_laptop(db, laptop_id=laptop_id)
-        if images is None or len(images) == 0:
-            raise ValueError
-        return images
-    except ValueError:
-        raise HTTPException(status_code=404, detail="Images not found for the given laptop ID")
-    except Exception:
-        raise HTTPException(status_code=500, detail="Internal Server Error")
-
-
-@router.get("/purchase/goods/asc/{rating}", response_model=List[schemas.Laptop])
-def read_laptops(rating: str = "", skip: int = 0, limit: int = 6,  db: Session = Depends(db.session)):
-    try:
-        laptops = crud.get_laptops_asc(db, skip=skip, limit=limit, rating = rating)
+        laptops = crud.get_laptops_asc(db, page=page, rating=rating)
         if laptops is None or len(laptops) == 0:
             raise ValueError
         return laptops
@@ -73,15 +39,3 @@ def read_laptops(rating: str = "", skip: int = 0, limit: int = 6,  db: Session =
         raise HTTPException(status_code=500, detail="Internal Server Error")
 
 
-
-@router.get("/purchase/goods/desc/{rating}", response_model=List[schemas.Laptop])
-def read_laptops(rating: str = "", skip: int = 0, limit: int = 6,  db: Session = Depends(db.session)):
-    try:
-        laptops = crud.get_laptops_desc(db, skip=skip, limit=limit, rating = rating)
-        if laptops is None or len(laptops) == 0:
-            raise ValueError
-        return laptops
-    except ValueError:
-        raise HTTPException(status_code=404, detail="Laptops not found")
-    except Exception:
-        raise HTTPException(status_code=500, detail="Internal Server Error")
