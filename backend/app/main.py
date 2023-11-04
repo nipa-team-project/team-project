79df571ccec392f3142cefd2bb4acf87fe907ec1
kkook1234 <57415a@naver.com>
2023-11-04 23:17:46 +0900

    feat: main expose_headers 수정

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,8 +2,9 @@ from dataclasses import asdict
 from db import database
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from routers import account_router
+from routers import account_router, laptop_info_list
 from core.config import conf
+
 
 def create_app():
     c = conf()
@@ -11,6 +12,7 @@ def create_app():
     conf_dict = asdict(c)
     database.db.init_app(app, **conf_dict)
     app.include_router(account_router.router)
+    app.include_router(laptop_info_list.router)
 
     # CORS 미들웨어 추가
     app.add_middleware(
@@ -19,12 +21,12 @@ def create_app():
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
+        expose_headers=["total_count"],
     )
-
+   
     return app
+
 
 app = create_app()
 
 app.include_router(account_router.router)
-
-
