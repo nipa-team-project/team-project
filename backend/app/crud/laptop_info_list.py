162b516b90d897cf194ca27af18b9bbea16e3720
찬익 박 <piaocanyi1997@gmail.com>
2023-11-04 18:19:59 +0900

    feat: query count 추가

--- backend/app/crud/laptop_info_list.py
+++ backend/app/crud/laptop_info_list.py
@@ -19,14 +19,34 @@ from schemas import laptop_info_list as schemas
 #         data_count
 #     )
 
-def get_laptops_desc(db: Session, page: int = 1, rating=str):
+# def get_laptops_desc(db: Session, page: int = 1, rating=str):
+#     page_size = 6
+#     skip = (page - 1) * page_size
+#     data_count = db.query(models.Laptop.laptop_info_list_id).count()
+#     query = db.query(models.Laptop).options(joinedload(models.Laptop.laptop_info_list_image))
+#
+#     if rating is not None:
+#         query = query.filter(models.Laptop.rank == rating)
+#
+#     return (
+#         query.order_by(models.Laptop.price.desc())
+#         .offset(skip)
+#         .limit(page_size)
+#         .all(),
+#         data_count
+#     )
+
+
+def get_laptops_desc(db: Session, page: int = 1, rating=None):
     page_size = 6
     skip = (page - 1) * page_size
-    data_count = db.query(models.Laptop.laptop_info_list_id).count()
+
     query = db.query(models.Laptop).options(joinedload(models.Laptop.laptop_info_list_image))
 
     if rating is not None:
         query = query.filter(models.Laptop.rank == rating)
+
+    data_count = query.count()
 
     return (
         query.order_by(models.Laptop.price.desc())
@@ -61,6 +81,8 @@ def get_laptops_asc(db: Session, page: int = 1, rating=str):
 
     if rating is not None:
         query = query.filter(models.Laptop.rank == rating)
+
+    data_count = query.count()
 
     return (
         query.order_by(models.Laptop.price.asc())
