fcb5192e50d89d80422fbc218dc48fcecb265188
찬익 박 <piaocanyi1997@gmail.com>
2023-11-04 18:26:13 +0900

    feat: cors add_middleware 추가

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,24 @@
 from dataclasses import asdict
+from fastapi.middleware.cors import CORSMiddleware
 from db import database
 from fastapi import FastAPI
 from routers import laptop_info_list
 from core.config import conf
 
+
 app = FastAPI()
+
+origins = [
+    # "http://localhost:3000",  # React 앱의 주소를 여기에 넣음
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    expose_headers=["X-Total-Count"]  # 사용자 정의 헤더 이름을 여기에 넣음
+)
 
 
 def list_app():
