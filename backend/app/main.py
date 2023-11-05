c6d3aefa81278020679d029c49d29163f567749c
찬익 박 <piaocanyi1997@gmail.com>
2023-11-05 23:52:01 +0900

    feat: 유저 리스트 main 수정

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,19 +1,2 @@
-from dataclasses import asdict
-from db import database
-from fastapi import FastAPI
-from routers import manager_router
-from core.config import conf
 
-
-def create_app():
-    c = conf()
-    app = FastAPI()
-    conf_dict = asdict(c)
-    database.db.init_app(app, **conf_dict)
-    app.include_router(manager_router.router)
-
-    return app
-
-
-app = create_app()
 
