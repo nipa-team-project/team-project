a86b6366bfb24ae0a2d7294f58ff0e2cf29e836a
kkook1234 <57415a@naver.com>
2023-11-09 18:36:57 +0900

    docs: 클래스 이름 충돌 수정

---
+++ backend/app/crud/laptop_crud.py
@@ -0,0 +1,11 @@
+from sqlalchemy.orm import Session, joinedload
+from schemas.laptop_schema import LaptopInput
+from models.laptop_model import Laptop2
+
+def laptop_input(db: Session, laptop: LaptopInput):
+    db_laptop = Laptop2(device_name=laptop.device_name, os=laptop.os, screen_size=laptop.screen_size,
+                            hardware=laptop.hardware, brand=laptop.brand)
+    db.add(db_laptop)
+    db.commit()
+
+
