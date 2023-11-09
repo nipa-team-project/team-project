7a68ebb95586334396fb63af34c00d7a8fb6b65f
leesuuuuumm <tnalsvjscl@naver.com>
2023-11-09 17:02:17 +0900

    feat: serving 기능 추가

--- backend/app/routers/laptop_sell_router.py
+++ backend/app/routers/laptop_sell_router.py
@@ -1,16 +1,17 @@
-from fastapi import APIRouter, Header, Depends, UploadFile
+from fastapi import APIRouter, Header, Depends, UploadFile, Form
 from starlette import status
 from schemas.laptop_info_schema import LaptopSellFormCreate
 from crud import laptop_sell_crud
 from db.database import db
 from sqlalchemy.orm import Session
-from typing import List
+from typing import List,Annotated
 
 
 router = APIRouter()
 
 
 @router.post("/sell", status_code=status.HTTP_200_OK)
-async def image_upload(sell_info: LaptopSellFormCreate, files: List[UploadFile],
-                       session: Session = Depends(db.session), token: str = Header(None)):
-    await laptop_sell_crud.laptop_sell_info_input(sell_info=sell_info, files=files,db=session, token=token)
+async def image_upload(device_name: Annotated[str, Form()], serial_number: Annotated[str, Form()], product_details: Annotated[str, Form()], step: Annotated[int,Form()],
+                       files: List[UploadFile], session: Session = Depends(db.session), token: str = Header(None)):
+   return await laptop_sell_crud.laptop_sell_info_input(device_name=device_name,serial_number=serial_number,product_details=product_details,
+                                                  step=step, files=files,db=session, token=token)
